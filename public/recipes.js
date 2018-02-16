b67028a58fe35b1e0e0a51f7084234a0a3f44355
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-02-16 15:17:55 -0500

    fixed DELETE request

--- public/recipes.js
+++ public/recipes.js
@@ -1,5 +1,8 @@
 // Accordian Animation
 $(document).on('click', 'button', function () {
+  $('html, body').animate({
+      scrollTop: $('.container').offset().top
+    }, 500);
     $(this).toggleClass("max").next().slideToggle(200);
 });
 
@@ -11,7 +14,7 @@ function getRecipeEntries(callbackFn) {
     type: 'GET',
     dataType: 'json',
 
-    success: function(data) {
+    success: data => {
       if(data) {
         let results = data;
         callbackFn(results);
@@ -25,14 +28,16 @@ function displayRecipeEntries(data) {
   for (index in data.recipes) {
     //console.log(data.recipes[index]);
        $('.container').append(`
+          <div id="${data.recipes[index].id}">
             <button class="btn">${data.recipes[index].title}</button>
             <div class="acd-content">
                 <p class="recipe-type">Recipe type: ${data.recipes[index].type}</p>
                 <p class="recipe-content">${data.recipes[index].content}</p>
                 <p class="recipe-calories">Calories: ${data.recipes[index].calories}</p>
                 <p class="recipe-author">Author: ${data.recipes[index].author}</p>
-                <button class="delete-btn">Delete</button>
+                <button class="delete-btn">Delete</button><button class="edit-btn">Edit</button>
             </div>
+          </div>
         `);
   }
 }
@@ -40,11 +45,13 @@ function displayRecipeEntries(data) {
 function deleteRecipeEntries(data) {
   for (index in data.recipes) {
     $('.delete-btn').on('click', function(event) {
+      let recipeId = $(this).parent('id');
+
       $.ajax({
         url: `/recipe/${data.recipes[index].id}`,
         type: 'DELETE',
         dataType: 'json',
-        contenttype: 'application/json',
+        contentType: 'application/json',
 
         success: data => {
           window.location = "/recipes";
